95a9a5242042ce954fdc20b540269d73be05fea0
MarioC17 <61722357+MarioC17@users.noreply.github.com>
2021-11-02 04:05:02 -0600

    Directory changes

--- mcfit_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from mcfit.models import Exercisetable
-
-class ExercisetableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Exercisetable
-        fields = ('name','muscle','equipment','description','benefits','instructions','video')
-        #fields = ('name')
--- mcfit_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ExerciseList
-
-app_name = 'mcfit_api'
-
-urlpatterns = [
-    path('', ExerciseList.as_view(), name='listcreate'),
-]
