c6e42efb49032a01dabbfeb2c2dfa8c86e90ac30
yaremchukc3 <77423894+yaremchukc3@users.noreply.github.com>
2021-11-15 14:24:23 -0700

    Add booking page

---
+++ react_frontend/mcfitapi/src/components/booking.js
@@ -0,0 +1,127 @@
+const auth_code = '489dc564dac34325825f75cc5e5d58fc'
+
+export async function generateSagendaToken()
+{
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "api_token");
+    urlencoded.append("api_token", auth_code); 
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+    
+    const r = await fetch("https://sagenda.net/api/token", requestOptions);
+    const response = await r.json();
+    
+    console.log(response['access_token']);
+    return response['access_token'];
+    
+}
+
+export async function verifyToken(token)
+{
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+    crossDomain: true
+    };
+    var response = await fetch("https://sagenda.net/api/v3/status/oauth", requestOptions);
+    //console.log(response.status);
+    return (response.status == 200)
+}
+
+export async function getBookableTimes(startDate, endDate)
+{
+    const token = await generateSagendaToken();
+    const status = await verifyToken(token);
+    console.log("Status: " + status);
+    var myHeaders = new Headers();
+    console.log(token);
+    myHeaders.append("Authorization", "Bearer " + token);
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    
+    var response = await fetch("https://sagenda.net/api/v3/events/" + startDate + "/" + endDate + "/6171f83f83926f4e50e9202c", requestOptions).then(reply => reply.json())
+    //console.log(response[0]);
+    return response;
+}
+
+async function getEventLock(eventID, token)
+{
+    console.log('Token ' + token)
+    console.log('Event: ' + eventID)
+    var myHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token
+    };
+    
+var raw = JSON.stringify({
+  "eventIdentifier": eventID,
+  "participants": 1
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+    
+    var response = await fetch("https://sagenda.net/api/v3/eventLocks", requestOptions).then(reply => reply.json());
+    console.log("Lock: " + response['identifier']);
+    return response;
+}
+
+export async function bookEvent(eventID, firstName, lastName, email)
+{
+    var token = await generateSagendaToken();
+    var lock = await getEventLock(eventID, token);
+    
+    var lockID = lock['identifier'];
+    var userID = lock['userIdentifier'];
+    console.log(lockID);
+
+var myHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token
+    };
+console.log(myHeaders);
+
+var raw = JSON.stringify({
+  "eventIdentifier": eventID,
+  "lockIdentifier": lockID,
+  "member": {
+    "email": "ckpy6@hotmail.com",
+    "firstName": firstName,
+    "lastName": lastName,
+    "participants": 1
+  }
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow',
+  crossDomain: true
+};
+    
+    var response = await fetch('https://sagenda.net/api/v3/events', requestOptions).then(reply => reply.text()).then(reply => console.log(reply));
+    //console.log(response);
+    //console.log(response.status);
+    //return response.status;
+    
+}
---
+++ react_frontend/mcfitapi/src/pages/booking.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { generateSagendaToken } from '../components/booking'
+import { getBookableTimes } from '../components/booking'
+import { bookEvent } from '../components/booking'
+
+var myToken;
+    
+export default class Booking extends React.Component
+{
+  state = {
+  weekend: true,
+  events: [{title: 'Test 1',start: '2021-11-12T13:00:00',end: '2021-11-12T14:00:00'}]
+}
+    
+  render() {
+    
+    return (
+      <FullCalendar
+        plugins={[ timeGridPlugin ]}
+        initialView="timeGridWeek"
+        slotMinTime="09:00:00"
+        slotMaxTime="17:00:00"
+        weekends={this.state.weekend}
+        events={this.state.events}
+        eventClick={this.handleEventClick}
+      />
+    )
+  }
+  
+  async componentDidMount()
+  {
+    //console.log("Rendered!")
+    var times = await getBookableTimes('2021-11-15','2021-11-19');
+    console.log(times)
+    for(let i = 0;i < times.length;i++){
+        times[i]['title'] = 'Appointment';
+        times[i]['start'] = times[i]['from'].substring(0,times[i]['from'].length-1)+":00";
+        times[i]['end'] = times[i]['to'].substring(0,times[i]['to'].length-1)+":00";
+        times[i]['backgroundColor'] = '#a0a0a0';
+        times[i]['borderColor'] = '#888888';
+        times[i]['textColor'] = '#101010'
+    }
+    console.log(times[0])
+    this.setState({weekend: false})
+    this.setState({events: times})
+  }
+  
+  handleEventClick = (clickInfo) => {
+      console.log("Event clicked: " +clickInfo.event._def.extendedProps.identifier);
+      console.log(clickInfo.event)
+      console.log("Date: " + clickInfo.event._instance.range.start)
+      var date = clickInfo.event._instance.range.start;
+      if(window.confirm("Are you sure you'd like to book an appointment for " + date + "?"))
+      {
+      var eventID = clickInfo.event._def.extendedProps.identifier;
+      console.log('ID: ' + eventID)
+      var success = bookEvent(eventID,'John','Doe','ckpy6@hotmail.com');
+      console.log("result" + success);
+    }
+  }
+}
