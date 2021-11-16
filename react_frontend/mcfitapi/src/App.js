243b377752c5bedbccd9a7d1b7c5f9e0298bb887
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 12:45:04 -0700

    slight changes to css

--- react_frontend/mcfitapi/src/App.js
+++ react_frontend/mcfitapi/src/App.js
@@ -5,8 +5,9 @@ import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-do
 import NotFoundPage from "./pages/404"
 import Login from "./pages/login"
 import Forms from "./pages/forms"
-import Exercise from "./pages/excercise"
+import Exercise from "./pages/exercise"
 import Signup from "./pages/Signup/signup"
+import Fitness from "./pages/fitness"
 import Signup2 from "./pages/Signup/signup2"
 import Signup3 from "./pages/Signup/signup3"
 import Signup4 from "./pages/Signup/signup4"
@@ -18,6 +19,7 @@ import Signup9 from "./pages/Signup/signup9"
 import Memberships from "./pages/memberships"
 import AboutMe from "./pages/aboutme"
 import Policy from "./pages/policy"
+import Booking from "./pages/booking"
 
 class App extends Component {
   render(){
@@ -29,17 +31,20 @@ class App extends Component {
           <Route exact path="/forms" component={Forms}/>
           <Route exact path="/exercise" component={Exercise}/>
           <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/fitness" component={Fitness}/>
           <Route exact path="/signup2" component={Signup2}/>
           <Route exact path="/signup3" component={Signup3}/>
           <Route exact path="/signup4" component={Signup4}/>
           <Route exact path="/signup5" component={Signup5}/>
           <Route exact path="/signup6" component={Signup6}/>
           <Route exact path="/signup7" component={Signup7}/>
-          \<Route exact path="/signup8" component={Signup8}/>
+          <Route exact path="/signup8" component={Signup8}/>
           <Route exact path="/signup9" component={Signup9}/>
+          <Route exact path="/signup8" component={Booking}/>
           <Route exact path="/memberships" component={Memberships}/>
           <Route exact path="/aboutme" component={AboutMe}/>
           <Route exact path="/policy" component={Policy}/>
+          <Route exact path="/booking" component={Booking}/>
           <Redirect to="/404"/>
         </Switch>
       </Router>
---
+++ react_frontend/mcfitapi/src/pages/aboutme.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+//Components
+import Header from '../components/HeaderW'
+import Footer from '../components/FooterW'
+import img from "../static/img/mikebw@1x.png"
+import sig from "../static/img/signature.png"
+//Style sheet
+import "./aboutme.css"
+
+export class aboutme extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor: "white", minHeight: "100vh"}}>
+                <Header/>
+                <div className="about-container">
+                    <div className="top-text">
+                        About Me
+                    </div>
+                    <div className="pic-area">
+                        <img src={img} alt=""/>
+                    </div>
+                    <div className="about-text">
+                        <div className="mike-text">
+                            Hi, I'm Mike!
+                        </div>
+                        <div className="par-text">
+                            “It is with great pleasure that I say I am a NAIT Personal Fitness Trainer Program graduate, which has helped me
+                             earn my CSEP Certified Personal Trainer and NASM Corrective Exercise Specialist certifications. <br />
+                            <br />
+                            Being active and playing sports has always been a huge part of my life. When I started my fitness journey, I was
+                            inspired to work hard in the gym when I began to see rewarding results. I want to be able to support that type of
+                            experience for others as I work beside them, and guide them on their own personal path to starting or maintaining
+                            a healthy lifestyle. <br />
+                            <br />I take pride in being a compassionate, well educated personal trainer who loves motivating people to work
+                            toward their goals while not just focusing on their physical goals, but to focus on improving their mindset and
+                            challenging them to grow in all aspects of their lives. Join me on a path to help you accomplish your goals,
+                            realize your full potential, and celebrate your successes.”
+                        </div>
+                        <div>
+                            <img style={{float: 'right', marginTop: "15%"}} src={sig} alt=""/>
+                        </div>
+                    </div>
+                    
+                </div>
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default aboutme
