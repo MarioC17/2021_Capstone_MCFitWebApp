5d6319cb89a78548e38e725c1dbf50e64720a559
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 19:25:19 -0700

    added logout button

--- react_frontend/mcfitapi/src/components/SidebarData.js
+++ react_frontend/mcfitapi/src/components/SidebarData.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { EventAvailable, Fastfood, FitnessCenter, Home, Payment, Settings } from '@material-ui/icons/';
+import LogoutIcon from '@mui/icons-material/Logout';
 import img from "../static/img/Logo.png";
 import { Box } from '@mui/material';
 import { Link } from "react-router-dom";
@@ -31,5 +32,9 @@ export const SidebarData = [
     {
         icon: <Settings/>,
         link: "/",
+    },
+    {
+        icon: <LogoutIcon/>,
+        link: "/"
     }
 ]
