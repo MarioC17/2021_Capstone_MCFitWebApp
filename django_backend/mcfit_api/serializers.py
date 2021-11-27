2cc2339072144bd6a38d6748491e5465c10d796d
MarioC17 <61722357+MarioC17@users.noreply.github.com>
2021-11-27 00:09:04 -0700

    reverted all custom log in commits

--- django_backend/mcfit_api/serializers.py
+++ django_backend/mcfit_api/serializers.py
@@ -1,18 +1,9 @@
 from rest_framework import serializers
 from mcfit.models import Exercisetable
-from djoser.serializers import UserCreateSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta:
-        model = User
-        fields = ('id','email','first_name','last_name','phone_number','emergency_contact','password','dob')
 
 class ExercisetableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercisetable
         fields = ('exercise_id','name','muscle','equipment','description','benefits','instructions','video')
-
+        #fields = ('name','muscle')
 
--- react_frontend/mcfitapi/src/pages/Signup/signup.jsx
+++ react_frontend/mcfitapi/src/pages/Signup/signup.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
-import { Button } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-//Components
-import Header from '../../components/HeaderW'
+import { createTheme } from '@mui/material/styles';
+import React, { useState } from "react";
 //Stylesheet
 import './signup.css';
+import Signup1 from './signup1';
+import Signup2 from './signup2';
+
+
 
 const theme = createTheme({
     palette: {
@@ -17,148 +16,58 @@ const theme = createTheme({
     },
   });
 
-export class signup extends Component {
-    constructor() {
-        super();
-    
-        this.state = {
-          firstname: "",
-          lastname: "",
-          username: "",
-          password: "",
-          confirmpassword: "",
-          email: ""
-        };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        let target = event.target;
-        let value = target.type === "checkbox" ? target.checked : target.value;
-        let name = target.name;
-    
-        this.setState({
-          [name]: value
-        });
-      }
-    
-      handleSubmit(event) {
-        event.preventDefault();
-        console.log("The form was submitted with the following data:");
-        console.log(this.state);
-      }
-    
-    render() {
-        return (
-            <div style={{backgroundColor: "white", minHeight: "100vh"}}>
-                <Header/>
-                <div class="container">
-                    <div className="a">
-                        <span className="head-text-style">
-                            Let’s connect! Please complete your <br />
-                            <br />
-                            information so we can get to know you.
-                        </span>
-                    </div>
-                    <div className="b">
-                        <form className="formFields" onSubmit={this.handleSubmit}>
-                            <div className="formField">
-                                <input
-                                type="firstname"
-                                id="firstname"
-                                className="formFieldInput"
-                                placeholder="First name"
-                                name="firstname"
-                                value={this.state.firstname}
-                                onChange={this.handleChange}
-                                />
-                            </div>
+const Signup = () => {
 
-                            <div className="formField">
-                                <input
-                                type="username"
-                                id="username"
-                                className="formFieldInput"
-                                placeholder="Username"
-                                name="username"
-                                value={this.state.username}
-                                onChange={this.handleChange}
-                                />
-                            </div>
+  const [step, setStep] = useState(1);
 
-                            <div className="formField">
-                                <input
-                                type="password"
-                                id="confirmpassword"
-                                className="formFieldInput"
-                                placeholder="Confirm Password"
-                                name="confirmpassword"
-                                value={this.state.confirmpassword}
-                                onChange={this.handleChange}
-                                />
-                            </div>
+  const nextStep = () => {
+    setStep(step + 1)
+  }
 
-                            <div className="formField">
-                                <ThemeProvider theme={theme}>
-                                    <Button color="neutral" 
-                                        variant="contained"
-                                        onClick={this.handleSubmit}>
-                                        Sign Up
-                                    </Button>
-                                </ThemeProvider>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="c">                        <form className="formFields" onSubmit={this.handleSubmit}>
-                            <div className="formField">
-                                <input
-                                type="lastname"
-                                id="lastname"
-                                className="formFieldInput"
-                                placeholder="Last name"
-                                name="lastname"
-                                value={this.state.lastname}
-                                onChange={this.handleChange}
-                                />
-                            </div>
+  const previousStep = () => {
+    setStep(step - 1)
+  }
 
-                            <div className="formField">
-                                <input
-                                type="password"
-                                id="password"
-                                className="formFieldInput"
-                                placeholder="Password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                />
-                            </div>
+  switch(step) {
+    case 1:
+      return(
+        <div>
+        <Signup1 nextStep = {nextStep} />
+        </div>
+    )
+    case 2:
+      return(
+        <Signup2 nextStep = {nextStep}/>
+    )
+    case 3:
+      return(
+        <h1>step 1</h1>
+    )
+    case 4:
+      return(
+        <h1>step 1</h1>
+    )
+    case 5:
+      return(
+        <h1>step 1</h1>
+    )
+    case 6:
+      return(
+        <h1>step 1</h1>
+    )
+    case 7:
+      return(
+        <h1>step 1</h1>
+    )
+    case 8:
+      return(
+        <h1>step 1</h1>
+    )
+    case 9:
+      return(
+        <h1>step 1</h1>
+    )
+  }
+  ;}
 
-                            <div className="formField">
-                                <input
-                                type="email"
-                                id="email"
-                                className="formFieldInput"
-                                placeholder="Email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                                />
-                            </div>
-
-                            <Link to="/" className="formFieldLink">
-                                <span className="sourcesanspro-normal-black-15px"><u>Already have an account? Sign In.</u></span>
-                            </Link>
-
-                            <button><Link to="/signup2">TEMP NAV DELETE LATER</Link></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default signup
+  export default Signup;
