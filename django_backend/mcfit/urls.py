4e0ebe654f677533453d14917efdf1db9c04f87f
MarioC17 <61722357+MarioC17@users.noreply.github.com>
2021-11-27 22:19:11 -0700

    Remove ignored files

--- django_backend/mcfit/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name='mcfit'
-
-urlpatterns = [
-    path('',TemplateView.as_view(template_name="mcfit/index.html")),
-]
--- django_backend/mcfit_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from mcfit.models import Exercisetable
-
-class ExercisetableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Exercisetable
-        fields = ('exercise_id','name','muscle','equipment','description','benefits','instructions','video')
-        #fields = ('name','muscle')
-
