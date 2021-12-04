1b650d7e3807dfc320bb9a7b4870ea10ddfcc8bc
JasperNakamura <jasperhibou@yahoo.com>
2021-12-03 17:06:20 -0700

    sidebar changes for trainer and login auth nav

--- react_frontend/mcfitapi/src/components/TrainerSidebar.js
+++ react_frontend/mcfitapi/src/components/TrainerSidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import { SidebarData } from './SidebarData';
+import { SidebarData } from './TrainerSidebarData';
 import { useHistory } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
